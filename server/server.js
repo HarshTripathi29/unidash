a031e1fa404eb8da7ea7ed09a915bba720796e3b
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-07-01 12:13:14 +0530

    update the server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,26 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const watchlistRoutes = require('./routes/Watchlist');
 const authRoutes = require('./routes/auth');
+const tokenRoutes = require('./routes/tokens');
+const path = require('path');
+const brc20WatchlistRouter = require('./routes/brc20Watchlist');
+
 
 const app = express();
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const CLIENT_URL = NODE_ENV === 'production' ? 'https://unidash-full.onrender.com' : 'http://localhost:5173';
+
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with the URL of your frontend if different
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'], // Allow PATCH method
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-  
-const port = 5000;
+  origin: CLIENT_URL,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+const port = process.env.PORT || 5000;
 const mongoURI = 'mongodb+srv://harshtripathi042:harsh123@cluster0.etqbz6r.mongodb.net/unidash';
 
 mongoose.connect(mongoURI)
@@ -23,6 +30,24 @@ mongoose.connect(mongoURI)
 app.use(bodyParser.json());
 app.use('/api/v1/watchlist', watchlistRoutes);
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/tokens', tokenRoutes);
+app.use('/api/v1/brc20Watchlist', brc20WatchlistRouter);
+
+//------deployment--------
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running successfully');
+  });
+}
+//------------
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
